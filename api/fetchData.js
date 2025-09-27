f630058e602451d8a07c12fb0252b35d935769c4
panwarayus-stack <panwarayus@gmail.com>
2025-09-27 12:01:53 +0530

    ctrl+s

---
+++ api/fetchData.js
@@ -0,0 +1,26 @@
+const { exec } = require("child_process");
+
+export default async function handler(req, res) {
+  const number = req.query.number;
+
+  if (!number) {
+    return res.status(400).json({ error: "Missing ?number= parameter" });
+  }
+
+  const curlCmd = `curl -s "https://osintx.info/API/aetherdemo.php?key=SANJ33T&type=mobile&term=${number}"`;
+
+  exec(curlCmd, (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).json({ error: error.message });
+    } else if (stderr) {
+      res.status(500).json({ error: stderr });
+    } else {
+      try {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(stdout);
+      } catch (e) {
+        res.status(500).json({ error: "Invalid JSON" });
+      }
+    }
+  });
+}
